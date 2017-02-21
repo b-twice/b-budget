8b00b1317f9536456bd3f2d5a4df12206ffef40d
Brian Brown <brbrowngeo@gmail.com>
2017-02-20 20:16:26 -0500

    server sync

--- server/sync.sh
+++ server/sync.sh
@@ -1,1 +1,1 @@
-rsync -a /home/bgeo/Documents/gitlab/bgeo-budget-web/dist/ bgeo@159.203.127.52:/var/www/budget-web
+rsync -a /home/bgeo/git/bgeo-budget-web/dist/ bgeo@159.203.127.52:/var/www/budget-web/
