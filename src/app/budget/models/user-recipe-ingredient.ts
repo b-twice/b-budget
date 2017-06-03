54c643164db48639c400e14ad685be407b716da2
Brian Brown <brbrowngeo@gmail.com>
2017-06-03 10:40:58 -0400

    measurement + pagenum

--- src/app/budget/models/user-recipe-ingredient.ts
+++ src/app/budget/models/user-recipe-ingredient.ts
@@ -6,6 +6,7 @@ export class UserRecipeIngredient {
         public count: number = 0,
         public weight: number = 0,
         public unit: string,
+        public measurement: string,
         public cost: number = 0,
         public costOrganic: number = 0,
         public costSeasonal: number = 0
