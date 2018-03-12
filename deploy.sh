78d47c6c15731850b243d31586fa35352bca628a
Brian Brown <brbrowngeo@gmail.com>
2018-03-12 19:57:50 -0400

    [update]

--- deploy.sh
+++ deploy.sh
@@ -4,4 +4,4 @@ set -x #echo on
 echo "Building package"
 node_modules/@angular/cli/bin/ng build --prod --aot 
 echo "Syncing package"
-rsync -a /var/lib/jenkins/workspace/budget-api-pipeline/dist /var/www/budget-web/
+rsync -a /var/lib/jenkins/workspace/budget-web-pipeline/dist /var/www/budget-web/
