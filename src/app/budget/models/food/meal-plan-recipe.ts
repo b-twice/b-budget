bf26396dfc3124c4538edec31788a0da2e2aea0f
Brian Brown <brbrowngeo@gmail.com>
2018-05-20 20:00:47 -0400

    add count to meal plan recipes

--- src/app/budget/models/food/meal-plan-recipe.ts
+++ src/app/budget/models/food/meal-plan-recipe.ts
@@ -7,6 +7,7 @@ export class MealPlanRecipe {
                 public cookbook: string,
                 public pageNumber: number = 0,
                 public servings: number = 0,
+                public count: number = 1,
                 public servingCost: number = 0,
                 public cost: number = 0,
                 public costOrganic: number = 0,
