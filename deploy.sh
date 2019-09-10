87b31896df9a75062df512f2ab634569553c165f
Brian Brown <bbrown@geodecisions.com>
2019-09-10 12:24:38 -0400

    switch to actions

--- deploy.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -x #echo on
-
-echo "Building package"
-node_modules/@angular/cli/bin/ng build --prod --aot 
-echo "Syncing package"
-rsync -a /var/lib/jenkins/workspace/budget-web-pipeline/dist/ /var/www/budget-web/
